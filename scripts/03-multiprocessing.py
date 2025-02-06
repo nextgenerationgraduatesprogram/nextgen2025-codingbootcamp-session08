e8e0d2951eba33915c03e8159d421c69be98809c
samcantrill <33347112+samcantrill@users.noreply.github.com>
2025-02-06 11:22:29 +1100

    mid workshop crisis

--- scripts/03-multiprocessing.py
+++ scripts/03-multiprocessing.py
@@ -1,13 +1,30 @@
 import time
+import timeit
 import random
+import numpy as np
 import multiprocessing as mp
+
+
+def matrix_multiply(A, B):
+    # Create the result matrix with zeros
+    output = np.zeros((A.shape[0], B.shape[1]))
+    # Perform matrix multiplication
+    for i in range(A.shape[0]):
+        for j in range(B.shape[1]):
+            output[i][j] = sum(A[i][k] * B[k][j] for k in range(A.shape[1]))
+    return output
+
 
 # lets define some task to perform
 def job_with_fixed_execution_time(x: int):
     """
     execution time is a fixed 1.0s
     """
-    time.sleep(1)
+    sz = 100
+    A = np.random.randn(sz,sz)
+    B = np.random.randn(sz,sz)
+    output = matrix_multiply(A, B)
+    # time.sleep(1)
     return x ** 2
 
 
@@ -15,7 +32,10 @@ def job_with_fixed_execution_time_and_two_args(x: int, y: int):
     """
     execution time is a fixed 1.0s
     """
-    time.sleep(1)
+    sz = 100
+    A = np.random.randn(sz,sz)
+    B = np.random.randn(sz,sz)
+    output = matrix_multiply(A, B)
     return x ** 2 + y ** 2
 
 
@@ -23,7 +43,10 @@ def job_with_variable_execution_time(x: int):
     """
     execution time is random
     """
-    time.sleep(random.random())
+    sz = random.randint(50, 150)
+    A = np.random.randn(sz,sz)
+    B = np.random.randn(sz,sz)
+    output = matrix_multiply(A, B)
     return x ** 2
 
 
@@ -31,18 +54,27 @@ def job_with_dependent_execution_time(t: int):
     """
     execution time depends on the input
     """
-    time.sleep(t)
+    t += 1
+    sz = random.randint(50 * t)
+    A = np.random.randn(sz,sz)
+    B = np.random.randn(sz,sz)
+    output = matrix_multiply(A, B)
     return t ** 2
 
 
 
-
 if __name__ == "__main__":
+    # benchmark job
+    N = 1
+    execution_time = timeit.timeit(lambda: job_with_fixed_execution_time(0), number=N)
+    print(f"Execution Time: {execution_time/N:.3f} seconds")
+
+
     # define a simple set of inputs to the job
-    inputs = [1,2,3,4,5]
+    inputs = range(1)
 
     # number of processes to distribute across
-    n_processes = 2
+    n_processes = 1
 
     # spawn the processes
     with mp.Pool(processes=n_processes) as pool:
@@ -50,6 +82,14 @@ if __name__ == "__main__":
         """
         synchronous mapping applies a function to an iterable and blocks until all
         results are available.
+
+        worker1: 
+        worker2: 
+        worker3: 
+        worker4: 
+        worker5: 
+        worker6: 
+
         """
         t = time.perf_counter_ns()
         outputs = pool.map(
@@ -59,6 +99,16 @@ if __name__ == "__main__":
         t = time.perf_counter_ns() - t
         print(f"{inputs} -> job -> {outputs}")
         print(f"`map` execution took {t/1e9:.3f} s\n")
+
+
+
+
+
+
+
+
+
+
 
 
         # # # map asychronously
